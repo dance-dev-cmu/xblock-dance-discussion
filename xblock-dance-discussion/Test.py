5ac2ad7ed845e2ebc5609b5588729b536561c729
DANCE dev <dance.dev.cmu@gmail.com>
2015-06-12 15:12:16 -0400

    Working test script for DB access with exception handling

--- xblock-dance-discussion/Test.py
+++ xblock-dance-discussion/Test.py
@@ -1,39 +1,48 @@
 from xblock.core import XBlock
 from xblock.fields import Scope, Integer, String
 import settings as s
-
-import mysql.connector as db_conn
+from datetime import date, datetime, timedelta
+import mysql.connector as db_connector
 import pkg_resources
-
-
-#@staticmethod
-#def resource_string(self, path):
-def resource_string(path):
-    """Handy helper for getting resources from our kit."""
-    data = pkg_resources.resource_string(__name__, path)
-    return data.decode("utf8")
-
-#def make_db_connection(self):
-def make_db_connection():
-    db = db_conn.connect(**s.database)
-    """
-     settings.py contains a dictionary of key value pairs that define the mysql user name + password, the host on
-     which the mysql user has been configured, the name of the database to use (the user specified here must be
-    granted access to it on the host specified) and whether warnings should raise exceptions (which will show up in
-    lms logs.)
-    """
-    #create_query = (self.resource_string(self, "./discussion_setup.sql"))
-    create_query = (resource_string( "./discussion_setup.sql"))
-    cursor = db.cursor()
-    cursor.execute(create_query)
-    test_query=("INSERT INTO discussion_table VALUES (1,-1,-1,'HELLO! FIRST COMMENT', -1,12/12/2015)")
-    cursor.execute(test_query) #-----> THIS DID NOT WORK!!! ALSO, FIGURE OUT HOW TO GET AUTOINCREMENT TO WORK!
-    cursor.close()
-    db.close()
+import sys
         
 user_id = ''
 parent_comment_id = -1 #This should be set in case the user clicks 'reply to an existing comment'
 thread_id = -1 #This should be fetched using the parent_comment_id to fetch it's thread_id
 comment = ''
+db = None #This is the DB connection. WE NEED TO ENSURE THIS IS CLOSED!
+
+def resource_string( path):
+    """Handy helper for getting resources from our kit."""
+    data = pkg_resources.resource_string(__name__, path)
+    return data.decode("utf8")
+
+def get_error_msg():
+    return(str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
+
+def exec_query(query, prefix_msg = ""):
+    cursor = db.cursor()
+    try:
+        cursor.execute(query)
+    except:
+        print(prefix_msg + " Error :" + get_error_msg()) # PROBABLY NEED TO REPLACE THIS WITH A LOGGING STATEMENT!
+    else:
+        db.commit()
+    cursor.close()
+
+
+def make_db_connection():
+    global db
+    db = db_connector.connect(**s.database)
+    """
+    settings.py contains a dictionary of key value pairs that define the mysql user name + password, the host on
+    which the mysql user has been configured, the name of the database to use (the user specified here must be
+    granted access to it on the host specified) and whether warnings should raise exceptions (which will show up in
+    lms logs.)
+    """
+    create_query = (resource_string( "./discussion_setup.sql"))
+    exec_query( create_query, "Initial Table Create Query")
+    test_query = ("INSERT INTO discussion_table (thread_id, user_id, comment, parent_id) VALUES (1, 11, 'Akash made this comment', 1)")
+    exec_query( test_query, "Table Insert Query")
 
 make_db_connection()
--- xblock-dance-discussion/xblock-discussion-dance.py
+++ xblock-dance-discussion/xblock-discussion-dance.py
@@ -1,10 +1,19 @@
 from xblock.core import XBlock
 from xblock.fields import Scope, Integer, String
 import settings as s
-
-import mysql.connector as db_conn
+import sys
+import mysql.connector as db_connector
 import pkg_resources
 
+
+"""
+TO DO:
+Add exception handling for ALL database operations.
+Replace console prints with logging statements
+Separate out functions into their own modules (ex. All DB functions into a separate DB module)
+Add more documentation when possible
+
+"""
 
 class DiscussionDance(XBlock):
 
@@ -18,6 +27,7 @@ class DiscussionDance(XBlock):
     parent_comment_id = -1 #This should be set in case the user clicks 'reply to an existing comment'
     thread_id = -1 #This should be fetched using the parent_comment_id to fetch it's thread_id
     comment = ''
+    db = None #This is the DB connection. WE NEED TO ENSURE THIS IS CLOSED!
 
     @staticmethod
     def resource_string(self, path):
@@ -25,16 +35,29 @@ class DiscussionDance(XBlock):
         data = pkg_resources.resource_string(__name__, path)
         return data.decode("utf8")
 
+    def get_error_msg(self):
+        return(str(sys.exc_info()[0]) + " " + str(sys.exc_info()[1]))
+
+    def exec_query(self,query, prefix_msg = ""):
+        cursor = self.db.cursor()
+        try:
+            cursor.execute(query)
+        except:
+            print(prefix_msg + " Error :" + self.get_error_msg(self)) # PROBABLY NEED TO REPLACE THIS WITH A LOGGING STATEMENT!
+        else:
+            self.db.commit()
+        cursor.close()
+
+
     def make_db_connection(self):
-        db = db_conn.connect(**s.database)
+        self.db = db_connector.connect(**s.database)
         """
-        settings.py contains a dictionary of key value pairs that define the mysql user name + password, the host on
+        settings.py (imported as s) contains a dictionary of key value pairs that define the mysql user name + password, the host on
         which the mysql user has been configured, the name of the database to use (the user specified here must be
         granted access to it on the host specified) and whether warnings should raise exceptions (which will show up in
         lms logs.)
         """
-        create_query = (self.resource_string(self, "./discussion_setup.sql"))
-        cursor = db.cursor()
-        cursor.execute(create_query)
-        db.close()
-
+        create_query = (self.resource_string( "./discussion_setup.sql"))
+        self.exec_query(self, create_query, "Initial Table Create Query")
+        test_query = ("INSERT INTO discussion_table (thread_id, user_id, comment, parent_id) VALUES (1, 11, 'Akash made this comment', 1)")
+        self.exec_query(self, test_query, "Table Insert Query")
